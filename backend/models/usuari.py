a05a07ac512905e4bf57af7473384242c0fa9cbe
AbdelSaad <izkdima13@hotmail.com>
2025-02-10 18:32:54 +0100

    1 fdhfd

---
+++ backend/models/usuari.py
@@ -0,0 +1,31 @@
+
+
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
+from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime, timezone
+
+Base = declarative_base()
+
+class Usuari(Base):
+    """
+    Model de dades per a la taula d'usuaris
+    """
+    __tablename__ = 'db_usuari'
+
+    id = Column(Integer, primary_key=True, index=True)
+    empresa = Column(Boolean, default=True)  # Indica si és una empresa
+    nom = Column(String(50), nullable=False)
+    cognoms = Column(String(50), nullable=False)
+    correu = Column(String(100), unique=True, nullable=False, index=True)
+    contrasenya = Column(Text, nullable=False)  # Hash de la contrasenya
+    data_registre = Column(DateTime, default=func.now())  # Data i hora de registre
+    ultim_canvi_contrasenya = Column(DateTime, default=func.now())  # Últim canvi de contrasenya
+    intents_fallits_login = Column(Integer, default=0)  # Intents fallits de login
+    bloquejat = Column(Boolean, default=False)  # Indica si el compte està bloquejat
+    baixa = Column(Boolean, default=False)  # Indica si el compte està donat de baixa
+    compte_verificat = Column(Boolean, default=False)  # Indica si el compte ha estat verificat
+
+    def nom_complet(self):
+        """Retorna el nom complet de l'usuari."""
+        return f"{self.nom} {self.cognoms}"
